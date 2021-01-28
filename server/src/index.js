f98a5317613aa33b684a22ab1b8de71a7f941df4
HigorAntonio <higorantonio.ha@gmail.com>
2021-01-28 16:12:27 -0300

    Setting Up Database & Server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,51 @@
+require('dotenv/config');
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
+const MONGODB = process.env.MONGODB;
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
+  type Query {
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then(({ url }) => {
+    console.log(`Server runnig at ${url}`);
+  });
